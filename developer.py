340a9696ec3b3ff69db9b2822c98f9b7e1e85681
Sahil Thakur <sahilgaucho@gmail.com>
2021-06-01 22:15:49 +0530

    first commit

---
+++ developer.py
@@ -0,0 +1,8 @@
+
+class Developer:
+    def __init__(self,id,team_id,name,phone_number):
+        self.id = id
+        self.team_id = team_id
+        self.name = name
+        self.phone_number = phone_number
+        
